8d7e60e9044763492c9386fc2cb4f16b4a6bec2b
Jack Henley <jack.d.henley@gmail.com>
2014-03-24 14:27:09 -0400

    Added model validation for pledge, specs passing

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Pledge do
 
+	describe 'validations' do
+
+		it 'is invalid without an amount' do
+			expect(Pledge.create(user_id: 1, gift_id: 1, owner: true)).to_not be_valid
+		end
+
+		it 'is valid when all required fields are entered' do
+			expect(Pledge.create(user_id: 1, gift_id: 1, amount: 34)).to be_valid
+		end
+	end
 end
