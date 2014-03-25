327c301d650f312721325d8890e40f05c97cc33a
Jack Henley <jack.d.henley@gmail.com>
2014-03-24 22:33:47 -0400

    Added passing specs for remaining new gift form validations

--- spec/features/user_adds_gift_spec.rb
+++ spec/features/user_adds_gift_spec.rb
@@ -31,5 +31,27 @@ feature 'User Signs In' do
 			expect(page).to have_content 'This field is required'
 		end
 
+		scenario 'Tries to unsuccessfully add a new gift without an end date' do
+			click_on 'Create New Gift Campaign'
+
+			fill_in 'Title for campaign', with: 'New computer for Steve'
+			fill_in 'Description of gift:', with: 'New macbook pro'
+			fill_in 'Reason for gift:', with: 'Steves birthday is coming up. Lets get him a new computer!'
+			fill_in 'Campaign goal:', with: '1,500'
+			click_on 'Create campaign!'
+			expect(page).to have_content 'This field is required'
+		end
+
+		scenario 'Tries to unsuccessfully add a new gift without a campaign goal' do
+			click_on 'Create New Gift Campaign'
+
+			fill_in 'Title for campaign', with: 'New computer for Steve'
+			fill_in 'Description of gift:', with: 'New macbook pro'
+			fill_in 'Reason for gift:', with: 'Steves birthday is coming up. Lets get him a new computer!'
+			fill_in 'Campaign duration:', with: '30'
+			click_on 'Create campaign!'
+			expect(page).to have_content 'This field is required'
+		end
+
 	end
 end
