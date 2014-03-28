2605ee35f4581d6f5e4ac6ab51f77be13a30d3d5
Jack Henley <jack.d.henley@gmail.com>
2014-03-28 09:19:22 -0400

    Added styling to new gift page, fixed jquery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery.validate
 //= require jquery.ui.all
+//= require bootstrap
 //= require_tree .
