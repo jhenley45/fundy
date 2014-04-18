6579bd481ba57f03c5dd5cc8191a39ce8e1b82c5
Jack Henley <jack.d.henley@gmail.com>
2014-04-18 10:34:29 -0400

    fixed bug in worker. Pledges are being successfully processed using resque background task!

--- app/workers/pledge_worker.rb
+++ app/workers/pledge_worker.rb
@@ -2,7 +2,7 @@ class PledgeWorker
 	@queue = :pledges
 
 	def self.perform(gift_id)
-		gift = gift.find(gift_id)
+		gift = Gift.find(gift_id)
 		gift.charge_gift_pledges
 		puts 'THIS IS WORKING'
 	end
