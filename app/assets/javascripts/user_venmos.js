6f2248c5bbc0c9f450c338820cf0609e47ac3eca
Jack Henley <jack.d.henley@gmail.com>
2014-04-28 00:09:45 -0400

    Added js to user campaign sidebar for sorting

--- app/assets/javascripts/user_venmos.js
+++ app/assets/javascripts/user_venmos.js
@@ -1,2 +1,20 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var Fundy = Fundy || {};
+
+$(document).ready(function() {
+	Fundy.addUserGiftSortClick();
+});
+
+Fundy.userCampaignSort = function(data_type) {
+	$('#user-gifts').children('li').hide();
+	$("li[data-" + data_type + "='true']").show();
+};
+
+Fundy.addUserGiftSortClick = function() {
+	$('.user-gift-sort').each(function() {
+		$(this).click(function() {
+			Fundy.userCampaignSort(this.id)
+		});
+	});
+};
+
+
